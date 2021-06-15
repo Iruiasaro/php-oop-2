bbef579d6154392f0749caf2a74d49bdf7440c17
Iruiasaro <brunettirosario@gmail.com>
2021-06-15 23:12:58 +0200

    INIZIALIZZAZIONE

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/class/Prodotto.php';
+require_once __DIR__ . '/class/Smartphone.php';
+require_once __DIR__ . '/class/User.php';
+require_once __DIR__ . '/class/Acquistato.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta nome="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_OOP_2</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
